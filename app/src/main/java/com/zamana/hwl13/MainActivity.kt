5a8cebaa4f1586b9831b7e0569fc0fa14f62c686
vzamana <v.zamana@itransition.com>
2022-04-25 20:11:10 +0300

    Added comments

--- app/src/main/java/com/zamana/hwl13/MainActivity.kt
+++ app/src/main/java/com/zamana/hwl13/MainActivity.kt
@@ -2,10 +2,12 @@ package com.zamana.hwl13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Main Activity for new project
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+//assadsa
+//end file
